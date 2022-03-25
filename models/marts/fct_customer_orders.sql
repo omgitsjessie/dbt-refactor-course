bfa6f27aa3876f0689d438b5a92e90c3708f1d40
jessie.mueller@gmail.com <jessie.mueller@gmail.com>
2022-03-25 18:40:12 +0000

    break up cte groupings

--- models/marts/fct_customer_orders.sql
+++ models/marts/fct_customer_orders.sql
@@ -2,50 +2,61 @@ with
 
 -- Import CTEs
 orders as (
+
     select * from {{ source('jaffle_shop', 'orders') }}
+
 ),
 
-customers as (
+base_customers as (
+
     select * from {{ source('jaffle_shop', 'customers') }}
+
 ),
 
 payments as (
+
     select * from {{ source('stripe', 'payment') }}
+
 ),
 -- Logical CTEs (subqueries, things that have a desired sequence)
 
--- Final CTE (often called final)
-
--- Simple Select Statement
-
-select 
-
-    orders.id as order_id,
-    orders.user_id as customer_id,
-    last_name as surname,
-    first_name as givenname,
-    first_order_date,
-    order_count,
-    total_lifetime_value,
-    round(amount/100.0,2) as order_value_dollars,
-    orders.status as order_status,
-    payments.status as payment_status
-
-from orders
-
-join (
-      
-      select 
+customers as (
+        
+    select 
         
         first_name || ' ' || last_name as name, 
         * 
+    
+    from base_customers
+
+), 
+
+a as (
       
-      from customers
+    select 
+    
+    row_number() over (
+        partition by user_id 
+        order by order_date, id
+    ) as user_order_seq,
+    *
+    
+    from orders
 
-) customers
-on orders.user_id = customers.id
+),
 
-join (
+b as (
+      
+    select 
+    
+    first_name || ' ' || last_name as name, 
+    * 
+    
+    from customers
+
+), 
+
+customer_order_history as (
 
     select 
         
@@ -90,26 +101,9 @@ join (
         
         array_agg(distinct a.id) as order_ids
 
-    from (
-      
-      select 
-        
-        row_number() over (partition by user_id order by order_date, id) as user_order_seq,
-        *
-      
-      from orders
-    ) a
+    from a
 
-    join ( 
-      
-      select 
-        
-        first_name || ' ' || last_name as name, 
-        * 
-      
-      from customers
-    
-    ) b
+    join b
     on a.user_id = b.id
 
     left outer join payments c
@@ -120,11 +114,37 @@ join (
 
     group by b.id, b.name, b.last_name, b.first_name
 
-) customer_order_history
-on orders.user_id = customer_order_history.customer_id
+),
 
-left outer join payments
+-- Final CTE (often called final)
+final as (
+    select 
 
-on orders.id = payments.orderid
+        orders.id as order_id,
+        orders.user_id as customer_id,
+        last_name as surname,
+        first_name as givenname,
+        first_order_date,
+        order_count,
+        total_lifetime_value,
+        round(amount/100.0,2) as order_value_dollars,
+        orders.status as order_status,
+        payments.status as payment_status
 
-where payments.status != 'fail'
+    from orders
+
+    join customers
+    on orders.user_id = customers.id
+
+    join customer_order_history
+    on orders.user_id = customer_order_history.customer_id
+
+    left outer join payments
+    on orders.id = payments.orderid
+
+    where payments.status != 'fail'
+)
+
+-- Simple Select Statement
+
+select * from final
